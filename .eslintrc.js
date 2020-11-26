ac95bb29fd45f81777062d553949659728fb69bf
EduardoAraD <araujocarlos893@gmail.com>
2020-11-26 15:18:54 -0300

    add eslint and prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,28 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-};
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["airbnb", "prettier", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    __DEV__: 'readonly'
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      tsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": ["warn", { extensions: [".tsx", ".ts"] }],
+    "import/prefer-default-export": "off",
+    "no-param-reassign": "off",
+    "no-console": ["error", { allow: ["tron"] }]
+  }
+}
