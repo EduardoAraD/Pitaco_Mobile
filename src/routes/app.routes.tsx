e67946dbe181ec046391907e775ed620dbb9e918
EduardoAraD <araujocarlos893@gmail.com>
2020-11-27 16:54:14 -0300

    Update AppRoutes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Dashboard from '../screens/app/Dashboard';
-import Pitaco from '../screens/app/Pitaco';
+import DashboardScreen from '../screens/app/Dashboard';
+import PitacoScreen from '../screens/app/Pitaco';
+import ChampionshipScreen from '../screens/app/Championship';
+import LeagueScreen from '../screens/app/League';
 
-const Drawer = createDrawerNavigator();
+import DrawerComponent from '../components/DrawerComponent'
+
+const Tab = createBottomTabNavigator();
+
+function TabNavigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Dashboard" component={DashboardScreen} />
+      <Tab.Screen name="Pitaco" component={PitacoScreen} />
+      <Tab.Screen name="Championship" component={ChampionshipScreen} />
+      <Tab.Screen name="League" component={LeagueScreen} />
+    </Tab.Navigator>
+  )
+}
+
+const Drawer = createDrawerNavigator()
 
 export default function AppRoutes() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Dashboard" component={Dashboard} />
-      <Drawer.Screen name="Pitaco" component={Pitaco} />
+    <Drawer.Navigator drawerContent={ props => <DrawerComponent {...props} />}>
+      <Drawer.Screen name="HomeDrawer" component={TabNavigation} />
     </Drawer.Navigator>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
+import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 
 export default function Routes() {
-  return <AppRoutes />;
+    return <AppRoutes />;
 }
