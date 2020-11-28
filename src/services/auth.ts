94d456eba8d6c323a2ef950892e0a7d636d15576
EduardoAraD <araujocarlos893@gmail.com>
2020-11-27 21:58:40 -0300

    create services

---
+++ src/services/auth.ts
@@ -0,0 +1,40 @@
+import { AxiosError, AxiosResponse } from 'axios'
+import api from './api'
+
+interface Response {
+    token: string,
+    user: {
+        name: string,
+        email: string,
+        points: number,
+        cravados: number
+    }
+}
+
+function signIn(email: string, password: string): Response {
+    
+    api.post("/login", { email, password }).then((resp: AxiosResponse) => {
+        console.log(resp.data)
+    }).catch( (error: AxiosError) => {
+        console.log( error.response?.data.errors)
+    })
+
+    const token = ''
+    const user = { name: 'SourhT', email, points: 2, cravados: 0 }
+
+    return { token, user };
+}
+
+function register(name:string, email: string, password: string, confirmPassword: string) {
+    
+    //api.post("/signIn")
+
+    const user = { name, email, points: 0, cravados: 0 }
+    
+    console.log(user)
+
+    return user;
+}
+
+
+export { signIn, register };
