61cde88e0db6095824dfe5c0310ab42fa5ec1589
EduardoAraD <araujocarlos893@gmail.com>
2020-11-29 13:37:12 -0300

    Update AuthRoutes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -4,13 +4,29 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../screens/auth/Login'
 import SignUp from '../screens/auth/SignUp'
 
+import colors from '../assets/colors'
+
 const Stack = createStackNavigator();
 
 export default function AuhtRoutes() {
     return (
-        <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Navigator initialRouteName="Login"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: colors.greenPrimary,
+                },
+                headerTintColor: colors.whitePrimary,
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 20,
+                },
+            }}
+        >
+            <Stack.Screen name="Login" component={Login}
+                options={{ headerShown: false }} />
+            <Stack.Screen name="SignUp" component={SignUp} 
+                options={{ title: 'Cadastre-se' }} 
+            />
         </Stack.Navigator>
     );
 }
