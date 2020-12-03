332f87c1882543828cfabf877a647f72b7418424
EduardoAraD <araujocarlos893@gmail.com>
2020-12-03 19:29:51 -0300

    Update Routes

--- src/routes/app/league.routes.tsx
+++ src/routes/app/league.routes.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import League from '../../screens/app/League'
+import LeagueShow from '../../screens/app/LeagueShow'
 import Header from '../../components/HeaderComponent'
 
 const Stack = createStackNavigator()
@@ -11,6 +12,8 @@ export default function LeagueRoute() {
         <Stack.Navigator>
             <Stack.Screen name='LeagueScreen' component={League}
                 options={{ header: () => <Header title='League' border={true} /> }} />
+            <Stack.Screen name='LeagueShowScreen' component={LeagueShow}
+                options={{ headerShown: false }} />
         </Stack.Navigator>
     )
 }
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -5,6 +5,7 @@ import Login from '../screens/auth/Login'
 import SignUp from '../screens/auth/SignUp'
 import ForgotPassword from '../screens/auth/ForgotPassword'
 import ResetPassword from '../screens/auth/ResetPassword'
+import Header from '../components/HeaderComponent';
 
 import colors from '../assets/colors'
 
@@ -27,11 +28,11 @@ export default function AuhtRoutes() {
             <Stack.Screen name='Login' component={Login}
                 options={{ headerShown: false }} />
             <Stack.Screen name='SignUp' component={SignUp} 
-                options={{ title: 'Cadastre-se' }} />
+                options={{ header: () => <Header title='Cadastre-se' back={true} border={true} /> }} />
             <Stack.Screen name='ForgotPassword' component={ForgotPassword}
-                options={{ title: 'Redefinir a senha' }} />
+                options={{ header: () => <Header title='Redefinir a senha' back={true} border={true} /> }} />
             <Stack.Screen name='ResetPassword' component={ResetPassword}
-                options={{ title: 'Redefinir a senha' }} />
+                options={{ header: () => <Header title='Redefinir a senha' back={true} border={true} /> }} />
         </Stack.Navigator>
     );
 }
