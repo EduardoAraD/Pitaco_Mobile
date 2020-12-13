229b97d243bfccd01d837333210c855922746174
EduardoAraD <araujocarlos893@gmail.com>
2020-12-12 21:33:57 -0300

    create function CreatePitacoMatch in pitacoservices

--- src/services/pitaco.ts
+++ src/services/pitaco.ts
@@ -15,7 +15,8 @@ interface PitacoMatch {
 
 async function getPitacoMatchToday(email: string): Promise<{data: PitacoMatch[], error: string}> {
     const date = new Date()
-    return await api.post('/pitacos-today/user', { email, date: date.toLocaleDateString() })
+    const dataString = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+    return await api.post('/pitacos-today/user', { email, date: dataString })
         .then((resp: AxiosResponse) => {
             const { matchs , pitacos } = resp.data as MatchPitacoResponse
             const matchPitaco = matchs.map((match) : PitacoMatch => {
@@ -38,10 +39,10 @@ async function getPitacoMatchRodada(email: string, championship: number, rodada:
             const { matchs, pitacos } = resp.data as MatchPitacoResponse
             const matchPitaco = matchs.map((match) : PitacoMatch => {
                 for(let i = 0; i < pitacos.length; i++) {
-                    const pitaco = pitacos[i]
+                    const pitaco = { ...pitacos[i], update: false } as Pitaco
                     if(match.id === pitaco.match.id) return { pitaco, match }
                 }
-                return { pitaco: {golsHome: '', golsAway: '', point: 0, exactScore: 0} as Pitaco, match }
+                return { pitaco: {golsHome: '', golsAway: '', point: 0, exactScore: 0, update: false} as Pitaco, match }
             })
             return { data: matchPitaco, error: '' }
         }).catch(( err: AxiosError) => {
@@ -50,4 +51,17 @@ async function getPitacoMatchRodada(email: string, championship: number, rodada:
         })
 }
 
-export { getPitacoMatchToday, getPitacoMatchRodada }
+interface PitacoRequest { golsHome: number, golsAway: number, id: number}
+
+async function createPitacoMatch(email: string, pitacos: PitacoRequest[]) {
+    return await api.post('/pitacos', {email, pitacos})
+        .then(( resp: AxiosResponse) => {
+            const response = resp.data as Pitaco[]
+            return { pitacos: response, error: ''}
+        }).catch(( err: AxiosError) => {
+            const error = err.response?.data.error
+            return { pitacos: [], error }
+        })
+}
+
+export { getPitacoMatchToday, getPitacoMatchRodada, createPitacoMatch }
