7969057596b143bc2938d3c3a9afcdc7b2c68ee5
EduardoAraD <araujocarlos893@gmail.com>
2020-12-15 16:05:28 -0300

    Update Context

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,160 +1,179 @@
-import React, { createContext, useState, useEffect, useContext } from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import * as auth from '../services/auth'
-import api from '../services/api'
+import * as auth from '../services/auth';
+import api from '../services/api';
 
-import { User } from '../models/User'
+import { User } from '../models/User';
 
-import ColorsTheme from '../assets/theme/theme'
-import colorsLight from '../assets/theme/light'
-import colorsDark from '../assets/theme/dark'
+import { ColorsTheme } from '../assets/theme/theme';
+import colorsLight from '../assets/theme/light';
+import colorsDark from '../assets/theme/dark';
+import { AuthContextData, init } from './authContextData';
 
-interface AuthContextData {
-    signed: boolean;
-    user: User | null;
-    loading: boolean;
-    themeDark: boolean;
-    theme: ColorsTheme;
-    championship: number;
-    currentRodada: number;
+const AuthContext = createContext<AuthContextData>(init());
 
-    signIn(email: string, password: string): Promise<string>;
-    signUp(name: string, email: string, password: string, confirmPassword: string, accertTerms: boolean): Promise<string>;
-    signOut(): void;
-    forgotPassword(email: string): Promise<{success: string, error: string}>;
-    resetPassword(codig: string, password: string, confirmPassword: string): Promise<{success: string, error: string}>;
-    onChangeThemeDark(): Promise<void>;
-    currentRodadaChampionship(rodadaId: number): void;
+interface PropsAuthProvider {
+  children: React.ReactNode;
 }
 
-const AuthContext = createContext<AuthContextData>({} as AuthContextData)
+export function AuthProvider({ children }: PropsAuthProvider) {
+  const [user, setUser] = useState<User | null>(null);
+  const [currentRodada, setCurrentRodada] = useState(1);
+  const [championship, setChampionship] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [themeDark, setThemeDark] = useState(false);
+  const [theme, setTheme] = useState<ColorsTheme>(colorsLight);
 
-export const AuthProvider: React.FC = ({children}) => {
-    const [user, setUser] = useState<User | null>(null)
-    const [currentRodada, setCurrentRodada] = useState(1)
-    const [championship, setChampionship] = useState(0)
-    const [loading, setLoading] = useState(true)
-    const [themeDark, setThemeDark] = useState(false)
-    const [theme, setTheme] = useState<ColorsTheme>(colorsLight)
+  useEffect(() => {
+    async function loadStorageData() {
+      const values = await AsyncStorage.multiGet([
+        '@Pitaco:email',
+        '@Pitaco:token',
+        '@Pitaco:theme',
+      ]);
 
-    useEffect(() => {
-        async function loadStorageData() {
-            const values = await AsyncStorage.multiGet(
-                ['@Pitaco:user', '@Pitaco:token', '@Pitaco:theme', '@Pitaco:championship'])
-            
-            const storageUser = values[0][1]
-            const storageToken = values[1][1]
-            const theme = values[2][1]
-            const championship = values[3][1]
+      const storageEmail = values[0][1];
+      const storageToken = values[1][1];
+      const storageTheme = values[2][1];
 
-            if(storageUser && storageToken && championship) {
-                setUser(JSON.parse(storageUser))
-                setChampionship(parseInt(championship))
-                api.defaults.headers.Authorization = `Bearer ${storageToken}`
-                if(theme === 'true'){
-                    setThemeDark(true)
-                    setTheme(colorsDark)
-                } else{
-                    setThemeDark(false)
-                    setTheme(colorsLight)
-                }
-            }
-            setLoading(false)
+      if (storageEmail && storageToken) {
+        api.defaults.headers.Authorization = `Bearer ${storageToken}`;
+
+        const { data, error } = await auth.initUserApp(storageEmail);
+        if (error === '') {
+          api.defaults.headers.Authorization = `Bearer ${data.token}`;
+          setChampionship(data.championship);
+          setUser(data.user);
+          if (storageTheme === 'true') {
+            setThemeDark(true);
+            setTheme(colorsDark);
+          } else {
+            setThemeDark(false);
+            setTheme(colorsLight);
+          }
         }
-
-        loadStorageData();
-    }, [])
-
-    async function signIn(email: string, password: string) {
-        const { data, error } = await auth.signIn(email, password);
-        if(data.user){
-            setChampionship(data.championship)
-            setUser(data.user)
-
-            api.defaults.headers.Authorization = `Bearer ${data.token}`
-
-            const userKey = ["@Pitaco:user", JSON.stringify(data.user)]
-            const tokenKey = ["@Pitaco:token", data.token]
-            const championshipKey = ['@Pitaco:championship', data.championship.toString()] 
-            const themeKey = ['@Pitaco:theme', 'false']
-
-            await AsyncStorage.multiSet([userKey, tokenKey, championshipKey, themeKey])
-            return ''
-        } else {
-            return error
-        }
+      }
+      setLoading(false);
     }
 
-    async function signUp(name: string, email: string, password: string, confirmPassword: string, accertTerms: boolean){
-        if(!accertTerms) return 'Termos não aceitos'
+    loadStorageData();
+  }, []);
 
-        const { data, error } = await auth.register(name, email, password, confirmPassword)
-        if(data.user){
-            setChampionship(data.championship)
-            setUser(data.user)
+  async function signIn(email: string, password: string) {
+    const { data, error } = await auth.signIn(email, password);
+    if (error === '') {
+      setChampionship(data.championship);
+      setUser(data.user);
 
-            api.defaults.headers.Authorization = `Bearer ${data.token}`
+      api.defaults.headers.Authorization = `Bearer ${data.token}`;
 
-            const userKey = ["@Pitaco:user", JSON.stringify(data.user)]
-            const tokenKey = ["@Pitaco:token", data.token]
-            const championshipKey = ['@Pitaco:championship', data.championship.toString()] 
-            const themeKey = ['@Pitaco:theme', 'false']
+      const emailKey = ['@Pitaco:email', data.user.email];
+      const tokenKey = ['@Pitaco:token', data.token];
+      const themeKey = ['@Pitaco:theme', 'false'];
 
-            await AsyncStorage.multiSet([userKey, tokenKey, championshipKey, themeKey])
-
-            return ''
-        } else {
-            return error
-        }
+      await AsyncStorage.multiSet([emailKey, tokenKey, themeKey]);
+      return '';
     }
+    return error;
+  }
 
-    function signOut(){
-        AsyncStorage.clear().then(() => {
-            setUser(null)
-            setChampionship(0)
-            setCurrentRodada(0)
-            setThemeDark(false)
-            setTheme(colorsLight)
-        })
-    }
+  async function signUp(
+    name: string,
+    email: string,
+    password: string,
+    confirmPassword: string,
+    accertTerms: boolean
+  ) {
+    if (!accertTerms) return 'Termos não aceitos';
 
-    async function onChangeThemeDark() {
-        const theme = !themeDark
-        setThemeDark(theme)
-        if(theme){
-            await AsyncStorage.setItem('@Pitaco:theme', 'true')
-            setTheme(colorsDark)
-        } else {
-            await AsyncStorage.setItem('@Pitaco:theme', 'false')
-            setTheme(colorsLight)
-        }
-        
-    }
-
-    async function forgotPassword(email: string) {
-        return await auth.forgotPassword(email)
-    }
-
-    async function resetPassword( email: string, password: string, confirmPassword: string) {
-        return await auth.resetPassword(email, password, confirmPassword)
-    }
-
-    function currentRodadaChampionship(rodadaId: number) {
-        setCurrentRodada(rodadaId)
-    }
-
-    return (
-        <AuthContext.Provider
-            value={{signed: !!user, user, loading, themeDark, theme, championship, currentRodada,
-                signIn, signUp, signOut, forgotPassword, resetPassword, onChangeThemeDark, currentRodadaChampionship}}>
-            {children}
-        </AuthContext.Provider>
+    const { data, error } = await auth.register(
+      name,
+      email,
+      password,
+      confirmPassword
     );
+    if (error === '') {
+      setChampionship(data.championship);
+      setUser(data.user);
+
+      api.defaults.headers.Authorization = `Bearer ${data.token}`;
+
+      const emailKey = ['@Pitaco:email', data.user.email];
+      const tokenKey = ['@Pitaco:token', data.token];
+      const themeKey = ['@Pitaco:theme', 'false'];
+
+      await AsyncStorage.multiSet([emailKey, tokenKey, themeKey]);
+
+      return '';
+    }
+    return error;
+  }
+
+  function signOut() {
+    AsyncStorage.clear().then(() => {
+      setUser(null);
+      setChampionship(0);
+      setCurrentRodada(0);
+      setThemeDark(false);
+      setTheme(colorsLight);
+    });
+  }
+
+  async function onChangeThemeDark() {
+    const booleanTheme = !themeDark;
+    setThemeDark(booleanTheme);
+    if (booleanTheme) {
+      await AsyncStorage.setItem('@Pitaco:theme', 'true');
+      setTheme(colorsDark);
+    } else {
+      await AsyncStorage.setItem('@Pitaco:theme', 'false');
+      setTheme(colorsLight);
+    }
+  }
+
+  async function forgotPassword(email: string) {
+    return auth.forgotPassword(email);
+  }
+
+  async function resetPassword(
+    email: string,
+    password: string,
+    confirmPassword: string
+  ) {
+    return auth.resetPassword(email, password, confirmPassword);
+  }
+
+  function currentRodadaChampionship(rodadaId: number) {
+    setCurrentRodada(rodadaId);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signed: !!user,
+        user,
+        loading,
+        themeDark,
+        theme,
+        championship,
+        currentRodada,
+        signIn,
+        signUp,
+        signOut,
+        forgotPassword,
+        resetPassword,
+        onChangeThemeDark,
+        currentRodadaChampionship,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-export function useAuth(){
-    const context = useContext(AuthContext)
+export function useAuth() {
+  const context = useContext(AuthContext);
 
-    return context;
+  return context;
 }
---
+++ src/contexts/authContextData.ts
@@ -0,0 +1,77 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import colorsLight from '../assets/theme/light';
+import { ColorsTheme } from '../assets/theme/theme';
+import * as auth from '../services/auth';
+import { User } from '../models/User';
+
+export interface AuthContextData {
+  signed: boolean;
+  user: User | null;
+  loading: boolean;
+  themeDark: boolean;
+  theme: ColorsTheme;
+  championship: number;
+  currentRodada: number;
+
+  signIn(email: string, password: string): Promise<string>;
+  signUp(
+    name: string,
+    email: string,
+    password: string,
+    confirmPassword: string,
+    accertTerms: boolean
+  ): Promise<string>;
+  signOut(): void;
+  forgotPassword(email: string): Promise<{ success: string; error: string }>;
+  resetPassword(
+    codig: string,
+    password: string,
+    confirmPassword: string
+  ): Promise<{ success: string; error: string }>;
+  onChangeThemeDark(): Promise<void>;
+  currentRodadaChampionship(rodadaId: number): void;
+}
+
+export function init(): AuthContextData {
+  return {
+    signed: false,
+    user: null,
+    loading: false,
+    themeDark: false,
+    theme: colorsLight,
+    championship: 0,
+    currentRodada: 0,
+    signIn: async (email: string, password: string) => {
+      await auth.signIn(email, password);
+      return '';
+    },
+    signUp: async (
+      name: string,
+      email: string,
+      password: string,
+      confirmPassword: string,
+      accertTerms: boolean
+    ) => {
+      if (accertTerms) return '';
+      await auth.register(name, email, password, confirmPassword);
+      return '';
+    },
+    signOut: () => {},
+    forgotPassword: async (email: string) => {
+      return auth.forgotPassword(email);
+    },
+    resetPassword: async (
+      codig: string,
+      password: string,
+      confirmPassword: string
+    ) => {
+      return auth.resetPassword(codig, password, confirmPassword);
+    },
+    onChangeThemeDark: async () => {
+      await AsyncStorage.setItem('@Pitaco:default', 'true');
+    },
+    currentRodadaChampionship: (rodadaId: number) => {
+      AsyncStorage.setItem('@Pitaco:default', rodadaId.toString());
+    },
+  };
+}
