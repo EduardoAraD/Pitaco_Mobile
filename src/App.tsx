300558305d0f4f8f26c03747db4e803f646bf83a
EduardoAraD <araujocarlos893@gmail.com>
2020-12-15 16:07:44 -0300

    Add @type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StatusBar } from 'react-native'
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { AuthProvider } from './contexts/auth'
+import { AuthProvider } from './contexts/auth';
 
 import Routes from './routes';
 
@@ -11,9 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <AuthProvider>
-        <StatusBar backgroundColor='#388E3C' barStyle='light-content' />
+        <StatusBar backgroundColor="#388E3C" barStyle="light-content" />
         <Routes />
       </AuthProvider>
     </NavigationContainer>
-  )
+  );
 }
