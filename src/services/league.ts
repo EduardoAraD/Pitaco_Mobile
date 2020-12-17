37f8655debec0b0f986427860ac7e66cff3edd1c
EduardoAraD <araujocarlos893@gmail.com>
2020-12-17 00:20:50 -0300

    create function get CommomLeague, delete League, get create and result Solicitation

--- src/services/league.ts
+++ src/services/league.ts
@@ -1,6 +1,9 @@
 import { AxiosError, AxiosResponse } from 'axios';
-import { initLeague, League } from '../models/League';
 import api from './api';
+
+import { initLeague, League } from '../models/League';
+import { Point } from '../models/Point';
+import { User } from '../models/User';
 
 import Trophy1Img from '../assets/images/trophy1.png';
 import Trophy2Img from '../assets/images/trophy2.png';
@@ -8,8 +11,6 @@ import Trophy3Img from '../assets/images/trophy3.png';
 import Trophy4Img from '../assets/images/trophy4.png';
 import Trophy5Img from '../assets/images/trophy5.png';
 import Trophy6Img from '../assets/images/trophy6.png';
-import { Point } from '../models/Point';
-import { User } from '../models/User';
 
 function logoTrophy(val: string) {
   switch (val) {
@@ -171,6 +172,32 @@ async function getLeagueGuest(
     });
 }
 
+async function getCommomLeagues(
+  email1: string,
+  email2: string
+): Promise<ListLeagueResponse> {
+  return api
+    .post('/commom-leagues', { email1, email2 })
+    .then((resp: AxiosResponse) => {
+      const response = resp.data;
+      const leagues: League[] = response.map((item: LeagueDB) => {
+        return {
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          dono: item.dono,
+          logo: logoTrophy(item.trophy),
+          points: item.points,
+        };
+      });
+      return { data: leagues, error: '' };
+    })
+    .catch((err: AxiosError) => {
+      const error = err.response?.data.error;
+      return { data: [], error };
+    });
+}
+
 async function getLeagues(championship: number): Promise<ListLeagueResponse> {
   return api
     .post('/leagues', { championship })
@@ -215,12 +242,85 @@ async function showLeague(id: number): Promise<LeagueResponse> {
     });
 }
 
+async function getSolitationLeague(
+  id: number
+): Promise<{ data: Point[]; error: string }> {
+  return api
+    .get(`/league-solicitation/${id}`)
+    .then((resp: AxiosResponse) => {
+      const response = resp.data;
+      return { data: response, error: '' };
+    })
+    .catch((err: AxiosError) => {
+      const error = err.response?.data.error;
+      return { data: [], error };
+    });
+}
+
+interface MessageResponse {
+  success: string;
+  error: string;
+}
+
+async function createSolicitation(
+  id: number,
+  email: string
+): Promise<MessageResponse> {
+  return api
+    .post(`/league-solicitation/${id}`, { email })
+    .then(() => {
+      return { success: 'Solicitação realizada com sucesso', error: '' };
+    })
+    .catch((err: AxiosError) => {
+      const error = err.response?.data.error;
+      return { success: '', error };
+    });
+}
+
+async function resultSolicitation(
+  id: number,
+  email: string,
+  result: string
+): Promise<MessageResponse> {
+  return api
+    .post('/result-solicitation', { id, email, result })
+    .then(() => {
+      if (result === 'true')
+        return { success: 'Pitaqueiro adicionado na liga.', error: '' };
+      return { success: 'Removido a solicitação do pitaqueiro.', error: '' };
+    })
+    .catch((err: AxiosError) => {
+      const error = err.response?.data.error;
+      return { success: '', error };
+    });
+}
+
+async function deleteLeague(
+  id: number,
+  email: string
+): Promise<MessageResponse> {
+  return api
+    .post(`/league-delete/${id}`, { email })
+    .then(() => {
+      return { success: 'Liga excluída com sucesso', error: '' };
+    })
+    .catch((err: AxiosError) => {
+      const error = err.response?.data.error;
+      return { success: '', error };
+    });
+}
+
 export {
   getLeaguePitaco,
   getLeagueHeartPitaco,
   getLeagueDono,
   getLeagueGuest,
+  getCommomLeagues,
   createLeague,
   getLeagues,
   showLeague,
+  deleteLeague,
+  getSolitationLeague,
+  createSolicitation,
+  resultSolicitation,
 };
