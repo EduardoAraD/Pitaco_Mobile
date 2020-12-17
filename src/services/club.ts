6edafb7fbd086486bb2ac79aad272eda6e1c868e
EduardoAraD <araujocarlos893@gmail.com>
2020-12-17 00:18:27 -0300

    create function chooseClub

---
+++ src/services/club.ts
@@ -0,0 +1,21 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import { initUser, User } from '../models/User';
+import api from './api';
+
+async function chooseClub(
+  email: string,
+  clubeId: number
+): Promise<{ data: User; error: string }> {
+  return api
+    .post('/choose-club', { email, clubeId })
+    .then((resp: AxiosResponse) => {
+      const response = resp.data;
+      return { data: response, error: '' };
+    })
+    .catch((err: AxiosError) => {
+      const error = err.response?.data.error;
+      return { data: initUser(), error };
+    });
+}
+
+export { chooseClub };
