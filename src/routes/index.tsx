9a85d80ea977724090b1589c62ad12ce81172f0d
EduardoAraD <araujocarlos893@gmail.com>
2020-12-22 22:42:01 -0300

    Update index in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,54 @@
 import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import Lottie from 'lottie-react-native';
 import { useAuth } from '../contexts/auth';
 
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
+
+import colors from '../assets/theme/light';
+
+import logoPitaco from '../assets/images/logo_pitaco_white.png';
+import animate from '../assets/animate/17378-soccer-loading.json';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.greenPrimary,
+  },
+  logo: {
+    height: 200,
+    width: 200,
+  },
+  viewAnimate: {
+    position: 'absolute',
+    bottom: 1,
+    right: 1,
+  },
+  animate: {
+    height: 100,
+    width: 100,
+  },
+});
 
 export default function Routes() {
   const { signed, loading } = useAuth();
 
   if (loading) {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#999" />
+      <View style={styles.container}>
+        <Image source={logoPitaco} style={styles.logo} resizeMode="contain" />
+        <View style={styles.viewAnimate}>
+          <Lottie
+            style={styles.animate}
+            resizeMode="contain"
+            source={animate}
+            autoPlay
+            loop
+          />
+        </View>
       </View>
     );
   }
