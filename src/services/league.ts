276bd79bdb231adb9b1d2d492832b2c097cf45dd
EduardoAraD <araujocarlos893@gmail.com>
2020-12-27 00:24:30 -0300

    Update LeagueServices

--- src/services/league.ts
+++ src/services/league.ts
@@ -2,7 +2,7 @@ import { AxiosError, AxiosResponse } from 'axios';
 import api from './api';
 
 import { initLeague, League } from '../models/League';
-import { Point } from '../models/Point';
+import { Point, initPoint } from '../models/Point';
 import { User } from '../models/User';
 
 import Trophy1Img from '../assets/images/trophy1.png';
@@ -31,77 +31,105 @@ function logoTrophy(val: string) {
   }
 }
 
+interface LeaguePointResponse {
+  data: {
+    league: League;
+    position: number;
+    point: Point;
+  };
+  error: string;
+}
+
 interface LeagueResponse {
   data: League;
   error: string;
 }
 
-async function getLeaguePitaco(id: number): Promise<LeagueResponse> {
+async function getLeaguePitaco(
+  championship: number,
+  email: string
+): Promise<LeaguePointResponse> {
   return api
-    .post('/league-pitaco', { id })
+    .post('/league-pitaco', { championship, email })
     .then((resp: AxiosResponse) => {
       const response = resp.data;
+      const { position } = response;
+      const { point } = response;
       const league: League = {
-        id: response.id,
-        name: response.name,
-        description: response.description,
-        dono: response.dono,
-        logo: logoTrophy(response.trophy),
-        points: response.points,
+        id: response.league.id,
+        name: response.league.name,
+        description: response.league.description,
+        dono: response.league.dono,
+        logo: logoTrophy(response.league.trophy),
+        points: response.league.points,
       };
-      return { data: league, error: '' };
+      return { data: { position, point, league }, error: '' };
     })
     .catch((err: AxiosError) => {
       const error = err.response?.data.error;
-      return { data: initLeague(), error };
+      return {
+        data: { league: initLeague(), point: initPoint(), position: -1 },
+        error,
+      };
     });
 }
 
 async function getLeagueHeartPitaco(
-  id: number,
-  clubeId: number
-): Promise<LeagueResponse> {
+  championship: number,
+  clubeId: number,
+  email: string
+): Promise<LeaguePointResponse> {
   return api
-    .post('/league-heartClub', { id, clubeId })
+    .post('/league-heartClub', { championship, clubeId, email })
     .then((resp: AxiosResponse) => {
       const response = resp.data;
+      const { position } = response;
+      const { point } = response;
       const league: League = {
-        id: response.id,
-        name: response.name,
-        description: response.description,
-        dono: response.dono,
-        logo: logoTrophy(response.trophy),
-        points: response.points,
+        id: response.league.id,
+        name: response.league.name,
+        description: response.league.description,
+        dono: response.league.dono,
+        logo: logoTrophy(response.league.trophy),
+        points: response.league.points,
       };
-      return { data: league, error: '' };
+      return { data: { position, point, league }, error: '' };
     })
     .catch((err: AxiosError) => {
       const error = err.response?.data.error;
-      return { data: initLeague(), error };
+      return {
+        data: { league: initLeague(), point: initPoint(), position: -1 },
+        error,
+      };
     });
 }
 
 async function getLeagueDono(
   championship: number,
   email: string
-): Promise<LeagueResponse> {
+): Promise<LeaguePointResponse> {
   return api
     .post('/league-dono', { championship, email })
     .then((resp: AxiosResponse) => {
       const response = resp.data;
+      const { position } = response;
+      const { point } = response;
       const league: League = {
-        id: response.id,
-        name: response.name,
-        description: response.description,
-        dono: response.dono,
-        logo: logoTrophy(response.trophy),
-        points: response.points,
+        id: response.league.id,
+        name: response.league.name,
+        description: response.league.description,
+        dono: response.league.dono,
+        logo: logoTrophy(response.league.trophy),
+        points: response.league.points,
       };
-      return { data: league, error: '' };
+      return { data: { position, point, league }, error: '' };
     })
     .catch((err: AxiosError) => {
       const error = err.response?.data.error;
-      return { data: initLeague(), error };
+      return {
+        data: { league: initLeague(), point: initPoint(), position: -1 },
+        error,
+      };
     });
 }
 
@@ -132,9 +160,31 @@ async function createLeague(
     });
 }
 
+interface ListLeaguePointResponse {
+  data: {
+    league: League;
+    position: number;
+    point: Point;
+  }[];
+  error: string;
+}
+
 interface ListLeagueResponse {
   data: League[];
   error: string;
+}
+
+interface LeaguePointDB {
+  point: Point;
+  position: number;
+  league: {
+    id: number;
+    name: string;
+    description: string;
+    dono: User;
+    trophy: string;
+    points: Point[];
+  };
 }
 
 interface LeagueDB {
@@ -149,22 +199,26 @@ interface LeagueDB {
 async function getLeagueGuest(
   championship: number,
   email: string
-): Promise<ListLeagueResponse> {
+): Promise<ListLeaguePointResponse> {
   return api
     .post('/league-guest', { championship, email })
     .then((resp: AxiosResponse) => {
       const response = resp.data;
-      const leagues: League[] = response.map((item: LeagueDB) => {
+      const leaguesResp = response.map((item: LeaguePointDB) => {
         return {
-          id: item.id,
-          name: item.name,
-          description: item.description,
-          dono: item.dono,
-          logo: logoTrophy(item.trophy),
-          points: item.points,
+          point: item.point,
+          position: item.position,
+          league: {
+            id: item.league.id,
+            name: item.league.name,
+            description: item.league.description,
+            dono: item.league.dono,
+            logo: logoTrophy(item.league.trophy),
+            points: item.league.points,
+          },
         };
       });
-      return { data: leagues, error: '' };
+      return { data: leaguesResp, error: '' };
     })
     .catch((err: AxiosError) => {
       const error = err.response?.data.error;
@@ -198,12 +252,29 @@ async function getCommomLeagues(
     });
 }
 
-async function getLeagues(championship: number): Promise<ListLeagueResponse> {
+interface LeaguePaginate {
+  limit: number;
+  page: number;
+  filter: string;
+  leagues: League[];
+  total: number;
+}
+
+async function getLeaguesPage(
+  championship: number,
+  page: number,
+  limit: number,
+  filter: string
+): Promise<{ data: LeaguePaginate; error: string }> {
   return api
-    .post('/leagues', { championship })
+    .post('/leagues-page', { championship, page, limit, filter })
     .then((resp: AxiosResponse) => {
       const response = resp.data;
-      const leagues: League[] = response.map((item: LeagueDB) => {
+      const pageRes = response.page;
+      const limitRes = response.limit;
+      const filterRes = response.filter;
+      const { total } = response;
+      const leagues: League[] = response.league.map((item: LeagueDB) => {
         return {
           id: item.id,
           name: item.name,
@@ -213,11 +284,23 @@ async function getLeagues(championship: number): Promise<ListLeagueResponse> {
           points: item.points,
         };
       });
-      return { data: leagues, error: '' };
+      return {
+        data: {
+          page: pageRes,
+          limit: limitRes,
+          filter: filterRes,
+          leagues,
+          total,
+        },
+        error: '',
+      };
     })
     .catch((err: AxiosError) => {
       const error = err.response?.data.error;
-      return { data: [], error };
+      return {
+        data: { limit: 1, page: 1, filter: '', total: 0, leagues: [] },
+        error,
+      };
     });
 }
 
@@ -239,6 +322,85 @@ async function showLeague(id: number): Promise<LeagueResponse> {
     .catch((err: AxiosError) => {
       const error = err.response?.data.error;
       return { data: initLeague(), error };
+    });
+}
+
+interface PointsLeague {
+  limit: number;
+  page: number;
+  points: Point[];
+  total: number;
+}
+
+interface PointLeagueResponse {
+  data: PointsLeague;
+  error: string;
+}
+
+async function showPointLeaguePage(
+  id: number,
+  page: number,
+  limit: number
+): Promise<PointLeagueResponse> {
+  return api
+    .post('/leagues-points', { id, page, limit })
+    .then((resp: AxiosResponse) => {
+      const response = resp.data;
+      const pageRes = response.page;
+      const limitRes = response.limit;
+      const filterRes = response.filter;
+      const { total, points } = response;
+      return {
+        data: {
+          page: pageRes,
+          limit: limitRes,
+          filter: filterRes,
+          points,
+          total,
+        },
+        error: '',
+      };
+    })
+    .catch((err: AxiosError) => {
+      const error = err.response?.data.error;
+      return {
+        data: { limit: 1, page: 1, filter: '', total: 0, points: [] },
+        error,
+      };
+    });
+}
+
+async function showPointLeagueHeartClubPage(
+  id: number,
+  clubeId: number,
+  page: number,
+  limit: number
+): Promise<PointLeagueResponse> {
+  return api
+    .post('/leagues-points', { id, clubeId, page, limit })
+    .then((resp: AxiosResponse) => {
+      const response = resp.data;
+      const pageRes = response.page;
+      const limitRes = response.limit;
+      const filterRes = response.filter;
+      const { total, points } = response;
+      return {
+        data: {
+          page: pageRes,
+          limit: limitRes,
+          filter: filterRes,
+          points,
+          total,
+        },
+        error: '',
+      };
+    })
+    .catch((err: AxiosError) => {
+      const error = err.response?.data.error;
+      return {
+        data: { limit: 1, page: 1, filter: '', total: 0, points: [] },
+        error,
+      };
     });
 }
 
@@ -317,8 +479,10 @@ export {
   getLeagueGuest,
   getCommomLeagues,
   createLeague,
-  getLeagues,
+  getLeaguesPage,
   showLeague,
+  showPointLeaguePage,
+  showPointLeagueHeartClubPage,
   deleteLeague,
   getSolitationLeague,
   createSolicitation,
