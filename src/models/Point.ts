74bd43e3b8489da57bf491fa5b7fe772d08a4052
EduardoAraD <araujocarlos893@gmail.com>
2020-12-27 00:24:10 -0300

    Create function initPoint

--- src/models/Point.ts
+++ src/models/Point.ts
@@ -1,7 +1,11 @@
-import { User } from './User';
+import { User, initUser } from './User';
 
 export interface Point {
   points: number;
   exactScore: number;
   user: User;
+}
+
+export function initPoint(): Point {
+  return { points: 0, exactScore: 0, user: initUser() };
 }
