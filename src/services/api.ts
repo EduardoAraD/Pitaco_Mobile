5c06173a11defaf0aec92999d41cb68f3ad715d9
EduardoAraD <araujocarlos893@gmail.com>
2021-01-22 16:33:56 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://appitacos-deploy.herokuapp.com',
 });
-// URLBASEAPI LOCAL: 'http://10.0.0.108:3333/'
+// URLBASEAPI LOCAL: 'http://1.9.2.1.6.8.0.1.0.9:3333' ip do pc local
 // URLBASEAPI HEROKU: 'https://appitacos-deploy.herokuapp.com'
 
 export default api;
